ae7aa786db474eca4f05f86d0b0768e05eafaa80
justt1n <t1nnguy3n0709@gmail.com>
2021-04-04 23:34:41 +0700

    AC Strange Table

--- A Strange Table/run.c
+++ A Strange Table/run.c
@@ -1,3 +1,41 @@
 #include <stdio.h>
+// cong thuc gia tri hang x = i*n + j + 1
+// cong thuc gia tri cot x = i + j*m + 1
 
-int
+/*
+Tim vi tri tu ma tran hang
+    x = i*m + j + 1
+<=> x - 1 = i*m + j
+<=> i = (x - 1)/m
+    j = (x - 1)%m
+*/
+
+/*
+Tim vi tri tu ma tran cot
+    x = j*m + i + 1
+<=> x - 1 = j*m + i
+<=> j = (x - 1)/m
+    i = (x - 1)%m
+*/
+
+void solve()
+{
+    long long int m, n, x;
+    scanf("%lld%lld%lld", &m, &n, &x);
+    long long int i, j;
+    i = (x - 1) / m;
+    j = (x - 1) % m;
+    long long int result = j * n + i + 1;
+    printf("%lld\n", result);
+}
+
+int main()
+{
+    int k;
+    scanf("%d", &k);
+    for (int it = 1; it <= k; it++)
+    {
+        solve();
+    }
+    return 0;
+}
